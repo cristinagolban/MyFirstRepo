60007179842b68800d52fa98434ee9034b308ff0
Cristina Golban <cristina.golban@amdaris.com>
2021-03-18 09:58:22 +0200

    added messages

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("aaaaaaa");
+            Console.WriteLine("i am on development");
         }
     }
 }
